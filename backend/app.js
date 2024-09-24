81d7ebb825e9cface35db6826c632b4cd62fef3c
Feriel Rabah <ferielrabah1@gmail.com>
2024-09-24 09:20:58 +0200

    Set up Sequelize ORM and MySQL connection

--- backend/app.js
+++ backend/app.js
@@ -2,27 +2,26 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mysql = require('mysql');
 
+const {Sequelize} = require('sequelize');
+
 
 dotenv.config()
 const app = express();
 
-const db = mysql.createConnection({
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT 
+    dialect: 'mysql',
+    port: process.env.DB_PORT
+});
 
-})
-
-db.connect((err)=>{
-    if(err){
-        console.log("Error connecting to db",err);
-        return;
-    }
-    console.log("Connected to the MYSQL database");
-
-})
+// Test the connection
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection to MySQL using Sequelize has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 app.listen(process.env.PORT,()=>{
     console.log('listening on port '+process.env.PORT);
 })
