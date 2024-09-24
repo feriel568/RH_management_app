4a3518b21877207f1dc57f7a3abd9f4d7db4ab1c
Feriel Rabah <ferielrabah1@gmail.com>
2024-09-24 09:13:34 +0200

    Database configuration

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mysql = require('mysql');
+
 
 dotenv.config()
 const app = express();
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT 
+
+})
+
+db.connect((err)=>{
+    if(err){
+        console.log("Error connecting to db",err);
+        return;
+    }
+    console.log("Connected to the MYSQL database");
+
+})
 app.listen(process.env.PORT,()=>{
     console.log('listening on port '+process.env.PORT);
 })
