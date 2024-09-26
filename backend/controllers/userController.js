40ffc02afd57a7c7a3969027377ef6d2ffe9fe5e
Feriel Rabah <ferielrabah1@gmail.com>
2024-09-26 18:19:50 +0200

    feat : login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,8 +43,49 @@ const registerUser = async (req, res) => {
       });
     }
   };
+
+
+
+  const loginUser = async (req, res) => {
+    try{
+      const {email, password} = req.body;
+      const user = await req.User.findOne({ email: email});
+
+      if(!user){
+        return res.status(404).json({message : 'User not found'});
+
+      }
+
+      const isPasswordValid = await bcrypt.compare(password , user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+
+      const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
+        expiresIn: '1h',
+      });
+  
+      res.status(200).json({
+        message: 'Login successful',
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+        token,
+      });
+    
+
+    }catch (error) {
+      console.error('Failed to log in:', error);
+      res.status(500).json({ message: 'Error during login', error: error.message });
+    }
+  }
+
   
 
   module.exports = {
     registerUser,
+    loginUser
   }
